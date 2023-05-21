12780608904d9b63269aa775fcde01512b3c7abb
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-05-21 12:05:29 +0700

    feat: display the project name in ASCII art

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use figlet_rs::FIGfont;
+
 fn main() {
-    println!("Hello, world!");
+    // Display the CLIM ASCII art in ANSI Shadow font
+    let ansi_shadow_font = FIGfont::from_file("resources/ANSIShadow.flf").unwrap();
+    let figure = ansi_shadow_font.convert("CLIM");
+    assert!(figure.is_some());
+    println!("{}", figure.unwrap());
 }
